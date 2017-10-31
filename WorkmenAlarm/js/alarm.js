d4f29de3b0eac6c6698f93632d03b67e4985054e
Viacheslav Sheviakov <viacheslav.sheviakov@nure.ua>
2017-11-01 00:27:26 +0200

    Interface finished for Workmen

--- WorkmenAlarm/js/alarm.js
+++ WorkmenAlarm/js/alarm.js
@@ -1,6 +1,30 @@
+var toDayOfWeek = function (day) {
+    if (day <= 0) {
+        console.error('Wrong value!');
+    }
+
+    var days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+    return days[day - 1];
+};
+
+var curDayOfWeek = toDayOfWeek(new Date().getDay());
+
+$('.clockpicker').clockpicker({
+    placement: 'bottom',
+    align: 'right',
+    donetext: 'Done'
+});
+
 setInterval(function() {
     var currentdate = new Date();
-    var timeLabel = currentdate.getHours() + ':' + currentdate.getMinutes();
 
-    document.getElementById("time").innerHTML = timeLabel;
+    var hours = currentdate.getHours();
+    var minutes = currentdate.getMinutes();
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+
+    var timeLabel = hours + ':' + minutes;
+
+    document.getElementById('time').innerHTML = timeLabel;
 }, 1000);
